25f61b849c3c28a604cb47e60718658500c369d4
JOMAR_RA <42526993+jomarbp@users.noreply.github.com>
2023-06-08 10:32:58 -0500

    Add files via upload

---
+++ error/error403.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Error 403 - Acceso Prohibido</title>
+  <style>
+    body {
+      background-color: #000;
+      color: #0F0;
+      font-family: "Courier New", monospace;
+      text-align: center;
+      padding: 20px;
+    }
+
+    h1 {
+      font-size: 50px;
+      margin-bottom: 30px;
+    }
+
+    p {
+      font-size: 20px;
+      margin-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h1 style="color: #f00;">ERROR 403</h1>
+  <p>Acceso Prohibido</p>
+  <p>
+    <img src="https://media.gq.com.mx/photos/5ed53829e30eef01aa919c84/1:1/w_4828,h_4828,c_limit/anonymous-y-sus-ataques-mas-importantes.jpg" width="500px" height="500px">
+  </p>
+  <p style="font-size: 16px; color: #0F0;">Has sido detectado. El sistema es alertado. Espere consecuencias.</p>
+  <p style="font-size: 16px; color: #0F0;">Su dirección IP ha sido registrada por motivos de seguridad.</p>
+</body>
+</html>
